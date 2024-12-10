10e6820e308d9b37d84d66550c97f40828a72844
Nahuel Vichenzzi <tnod444@gmail.com>
2024-12-10 17:58:21 -0300

    Update client.tsx

--- src/app/supabase/client.tsx
+++ src/app/supabase/client.tsx
@@ -3,7 +3,7 @@
 import { createClient } from "@supabase/supabase-js";
 
   const url = process.env.NEXT_PUBLIC_SECRET_API_URL
-  const key = process.env.NEXT_PUBLIC_AUTH_SECRET_API_KEY ? process.env.NEXT_PUBLIC_AUTH_SECRET_API_KEY : "https://txupjiyrorohzerojpun.supabase.co"
-const supabase = createClient(url ? url : 'http://no.url.com', key);
+  const key = process.env.NEXT_PUBLIC_AUTH_SECRET_API_KEY ? process.env.NEXT_PUBLIC_AUTH_SECRET_API_KEY : "null"
+const supabase = createClient(url ? url : 'https://txupjiyrorohzerojpun.supabase.co', key);
 
 export default supabase;
